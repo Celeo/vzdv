560dd50da3fa1997430c1ba32d024c4739701479
Celeo <mattboulanger@fastmail.com>
2024-11-16 22:14:26 -0800

    Whoops

--- vzdv-tasks/src/main.rs
+++ vzdv-tasks/src/main.rs
@@ -39,21 +39,21 @@ async fn main() {
     let roster_handle = {
         let db = db.clone();
         tokio::spawn(async move {
-            // debug!("Waiting 15 seconds before starting roster sync");
-            // time::sleep(Duration::from_secs(15)).await;
-            // loop {
-            //     info!("Querying roster");
-            //     match roster::update_roster(&db).await {
-            //         Ok(_) => {
-            //             info!("Roster update successful");
-            //         }
-            //         Err(e) => {
-            //             error!("Error updating roster: {e}");
-            //         }
-            //     }
-            //     debug!("Waiting 2 hours for next roster sync");
-            //     time::sleep(Duration::from_secs(60 * 60 * 2)).await;
-            // }
+            debug!("Waiting 15 seconds before starting roster sync");
+            time::sleep(Duration::from_secs(15)).await;
+            loop {
+                info!("Querying roster");
+                match roster::update_roster(&db).await {
+                    Ok(_) => {
+                        info!("Roster update successful");
+                    }
+                    Err(e) => {
+                        error!("Error updating roster: {e}");
+                    }
+                }
+                debug!("Waiting 2 hours for next roster sync");
+                time::sleep(Duration::from_secs(60 * 60 * 2)).await;
+            }
         })
     };
 
@@ -61,42 +61,42 @@ async fn main() {
         let config = config.clone();
         let db = db.clone();
         tokio::spawn(async move {
-            // debug!("Waiting 30 seconds before starting activity sync");
-            // time::sleep(Duration::from_secs(30)).await;
-            // for index in 0u64.. {
-            //     /*
-            //      * Update everyone on a 6 hour schedule (15 minutes * 24 ticks = 6 hours).
-            //      * This update makes sure that everyone's data is accurate.
-            //      */
-            //     if index % 24 == 0 {
-            //         info!("Updating all activity");
-            //         match activity::true_up_all_controllers_activity(&config, &db).await {
-            //             Ok(_) => {
-            //                 info!("Full activity update successful");
-            //             }
-            //             Err(e) => {
-            //                 error!("Error updating full activity: {e}");
-            //             }
-            //         }
-            //     } else {
-            //         /*
-            //          * Update online controllers every 15 minutes.
-            //          * This update might introduce small deltas in total time, but updates much
-            //          * faster for controllers that are actively controlling.
-            //          */
-            //         info!("Online controller activity check");
-            //         match activity::update_online_controller_activity(&config, &db).await {
-            //             Ok(_) => {
-            //                 info!("Partial activity update successful");
-            //             }
-            //             Err(e) => {
-            //                 error!("Error updating partial activity: {e}");
-            //             }
-            //         }
-            //     }
-            //     debug!("Waiting 15 minutes for next activity sync tick");
-            //     time::sleep(Duration::from_secs(60 * 15)).await;
-            // }
+            debug!("Waiting 30 seconds before starting activity sync");
+            time::sleep(Duration::from_secs(30)).await;
+            for index in 0u64.. {
+                /*
+                 * Update everyone on a 6 hour schedule (15 minutes * 24 ticks = 6 hours).
+                 * This update makes sure that everyone's data is accurate.
+                 */
+                if index % 24 == 0 {
+                    info!("Updating all activity");
+                    match activity::true_up_all_controllers_activity(&config, &db).await {
+                        Ok(_) => {
+                            info!("Full activity update successful");
+                        }
+                        Err(e) => {
+                            error!("Error updating full activity: {e}");
+                        }
+                    }
+                } else {
+                    /*
+                     * Update online controllers every 15 minutes.
+                     * This update might introduce small deltas in total time, but updates much
+                     * faster for controllers that are actively controlling.
+                     */
+                    info!("Online controller activity check");
+                    match activity::update_online_controller_activity(&config, &db).await {
+                        Ok(_) => {
+                            info!("Partial activity update successful");
+                        }
+                        Err(e) => {
+                            error!("Error updating partial activity: {e}");
+                        }
+                    }
+                }
+                debug!("Waiting 15 minutes for next activity sync tick");
+                time::sleep(Duration::from_secs(60 * 15)).await;
+            }
         })
     };
 
